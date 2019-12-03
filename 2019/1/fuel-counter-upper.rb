5ce262176dc9dc399f9af6806ea2c1e2fe980561
Scott Zetlan <szetlan@zola.com>
2019-12-03 13:33:56 -0500

    Complete day 1

--- 2019/1/fuel-counter-upper.rb
+++ 2019/1/fuel-counter-upper.rb
@@ -9,4 +9,16 @@ end
 
 total_fuel = ARGV.map{|mass| fuel_needed(mass.to_i)}.reduce(:+)
 
-print "Total fuel: #{total_fuel}\n"
+print "Part one: Total fuel: #{total_fuel}\n"
+
+really_total_fuel = ARGV.map do |mass|
+    module_fuel = 0
+    current_fuel_needed = fuel_needed(mass.to_i)
+    while current_fuel_needed > 0 do
+        module_fuel += current_fuel_needed
+        current_fuel_needed = fuel_needed(current_fuel_needed)
+    end
+    module_fuel
+end.reduce(:+)
+
+print "Part two: real total: #{really_total_fuel}\n"
